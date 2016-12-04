05101119a15bd1caf252fa4a9bf56a01315f29a9
Jake Schoolmeesters <j.schoolmee@gmail.com>
2016-12-03 20:53:40 -1000

    finished section 2-2

--- pen/01-HTML-CSS/start/js/main.js
+++ pen/01-HTML-CSS/start/js/main.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
 
-	
+	// move the pend body up to connect with the first part
+	TweenMax.set('.part3', {y: -572});
+
+	// hide all headings and text
+	// TweenMax.set('h2', {autoAlpha: 0});
+	// TweenMax.set('p', {autoAlpha: 0});
+	TweenMax.set(['.parts h2, .parts p'], {autoAlpha: 0});
+
+	//create a tween that will move the pen boddy back to its original css position
+	var bodyToStart = TweenMax.to('.part3', 1, {y: 0, ease:Linear.easeNone, pause: true});
+
+	//Init ScrollMagic Controller
+
+	var controller = new ScrollMagic.Controller();
+
+	var bodyToStarScene = new ScrollMagic.Scene ({
+		triggerElement: ('.part1'),
+		triggerHook: 1,
+		offset: 287,
+		duration: 572
+	})
+	.setTween(bodyToStart)
+	.addIndicators()
+	.addTo(controller)
+	;
 
 });
